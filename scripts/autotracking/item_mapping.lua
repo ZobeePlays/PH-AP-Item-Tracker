705da8b7a0f6f9dac343003dc04ffa5ff214dd21
ZobeePlays <159688147+ZobeePlays@users.noreply.github.com>
2025-07-21 15:08:51 -0500

    Add files via upload
    tracker base

---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,4 @@
+ITEM_MAPPING = {
+	[0000000] = {{"silly", "toggle"}},
+	[0000001] = {{"funny", "toggle"}},
+}
---
+++ scripts/autotracking/location_mapping.lua
@@ -0,0 +1,4 @@
+LOCATION_MAPPING = {
+	[0000000] = {"@Dee Ess Eye/Brute Chest"},
+	[0000001] = {"@Dee Ess Eye/Blow in Microphone Chest"},
+}
